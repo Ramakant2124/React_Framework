82436ec38f7eacaff21422cea572a149288b476f
Ramakant Chaudhari <chaudhariramakant754@gmail.com>
2026-03-08 22:42:41 +0530

    Add files via upload

---
+++ pro4_varebel_img/src/App.jsx
@@ -0,0 +1,16 @@
+// Vrebales
+function App() {
+     const name ="ramakant"
+  return (
+
+      <div>
+       <h3>--App--</h3>
+       <p>num :{100}</p>
+       <p>name :{name}</p>
+      <img src="https://images.unsplash.com/photo-1769283039270-a8c2da8a80b0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM2fDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D"/>
+      </div>
+ 
+  )
+}
+
+export default App
